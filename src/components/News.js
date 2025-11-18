64f6c7d0aacd0e347f48bfe7ce166d53caaf1083
bhaveshrordev <salujabhavesh24@gmail.com>
2025-11-18 22:27:35 +0530

    designed news item as cards

--- src/components/News.js
+++ src/components/News.js
@@ -5,8 +5,23 @@ export default class News extends Component {
   render() {
     return (
       <div>
-        This is a news Component
-        <NewsItem />
+        <div className="container my-3">
+          <h2>TaazaKhabar - Top Headlines</h2>
+          <div className="row">
+            <div className="col-md-4">
+              <NewsItem title="I am Tite" description="I am a descripiton" />
+            </div>
+
+            <div className="col-md-4">
+              <NewsItem title="I am Tite" description="I am a descripiton" />
+            </div>
+
+            <div className="col-md-4">
+              <NewsItem title="I am Tite" description="I am a descripiton" />
+            </div>
+
+          </div>
+        </div>
       </div>
     )
   }
